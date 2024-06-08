d780b9d4158074045f0e9f38861db30120902d65
brucezhengxinhong <bruce.zhengxh@gmail.com>
2024-06-08 21:05:05 +0800

    update config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,8 @@ export default defineNuxtConfig({
       publicDir: "./docs",
     },
   },
-  router: {
-    base: "/",
-  },
+  // app: {
+  //   buildAssetsDir: "/docs/",
+  // },
+  ssr: false,
 });
